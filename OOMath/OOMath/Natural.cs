d62d602736fefcd677fa5d115d7c39e63314404f
SLaks <Schabse@GMail.com>
2011-11-02 11:31:51 -0400

    Arithmetic

--- OOMath/OOMath/Natural.cs
+++ OOMath/OOMath/Natural.cs
@@ -3,6 +3,9 @@
 		public static readonly Natural Zero = new Natural();
 		public static readonly Natural One = new Natural(Zero);
 		public static readonly Natural Two = new Natural(One);
+		public static readonly Natural Three = new Natural(Two);
+		public static readonly Natural Four = new Natural(Three);
+		public static readonly Natural Five = new Natural(Four);
 
 		private Natural() { }	//Creates Zero
 
@@ -29,5 +32,27 @@
 		public static bool operator !=(Natural a, Natural b) { return !(a == b); }
 		public static bool operator ==(Natural a, Natural b) { return object.Equals(a, b); }
 		#endregion
+
+		#region Comparisons
+		public static bool operator <(Natural a, Natural b) {
+			if (a == null) return false;
+			//Count down from a - 1.  If we meet b on the way, b < a.
+			for (var i = a.Previous; i != Zero; i = i.Previous)
+				if (i == b)
+					return true;
+			return false;
+		}
+
+		public static bool operator <=(Natural a, Natural b) { return a == b || a < b; }
+		public static bool operator >(Natural a, Natural b) { return !(a <= b); }
+		public static bool operator >=(Natural a, Natural b) { return !(a < b); }
+		#endregion
+
+		public static Natural operator +(Natural a, Natural b) {
+			Natural result = a;
+			for (var i = b; i != Zero; i = i.Previous)
+				result = new Natural(result);
+			return result;
+		}
 	}
 }
